4f22faa850c4ef4d46cf0c4939564a27103eeb9b
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2022-01-12 14:06:14 +0900

    Initial Commit

---
+++ MultiTargetSPMSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiTargetSPMSample
+//
+//  Created by Yusuke Hasegawa on 2022/01/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultiTargetSPMSample/MultiTargetSPMSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiTargetSPMSampleApp.swift
+//  MultiTargetSPMSample
+//
+//  Created by Yusuke Hasegawa on 2022/01/12.
+//
+
+import SwiftUI
+
+@main
+struct MultiTargetSPMSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
