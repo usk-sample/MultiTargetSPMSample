08637691915659f799cf4217c89b79d4b9fb55e8
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2022-01-12 16:31:50 +0900

    feat: remove existing target

--- Infra/Package.swift
+++ Infra/Package.swift
@@ -8,9 +8,6 @@ let package = Package(
     products: [
         
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "Infra",
-            targets: ["Infra"]),
         .library(name: "Api", targets: ["Api"]),
         .library(name: "Store", targets: ["Store"])
 
@@ -23,9 +20,6 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Infra",
-            dependencies: []),
-        .target(
             name: "Api",
             dependencies: []),
         .target(
@@ -33,6 +27,6 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "InfraTests",
-            dependencies: ["Infra"]),
+            dependencies: ["Api", "Store"]),
     ]
 )
