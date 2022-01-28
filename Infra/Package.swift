18296d2536053d4f7971119b8ea1ba195bbb3ca5
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2022-01-28 13:15:53 +0900

    feat: add dependency to API

--- Infra/Package.swift
+++ Infra/Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Api",
-            dependencies: []),
+            dependencies: ["Alamofire"]),
         .target(
             name: "Store",
             dependencies: []),
